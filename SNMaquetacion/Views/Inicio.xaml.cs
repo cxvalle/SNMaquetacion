da592e91b20d8973547fe4b0e16ea104c3ef72cc
Cristian Valle <cvalle@panasa.com.ec>
2020-09-08 09:23:37 -0500

    Agregar archivos de proyecto.

---
+++ SNMaquetacion/Views/Inicio.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SNMaquetacion.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private async void Btn_Mostra_Maq(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Maquetacion());
+        }
+
+    }
+
+
+}
